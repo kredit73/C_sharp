d91d228c0f3e554e183a5b4c0012319d086d66c8
Marat <kredit73@mail.ru>
2022-12-08 12:53:17 +0300

    Добавлены решения 13,19,10

---
+++ Task19/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа проверяет пятизначное число на палиндромом
+Console.WriteLine("Input integer: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n>9999 & n<100000)
+{
+    int a = n/10000;
+    int b = n%10;
+    int c = (n/1000)%10;
+    int d = (n%100)/10;
+    if (a == b && c == d)
+    {
+        Console.WriteLine("Integer is Palindrome! ");
+    }
+    else Console.WriteLine("Integer is not Palindrome! ");
+
+}
+else Console.WriteLine("Was entered not a five-digit number!");
