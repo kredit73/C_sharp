2389052eaf630c505d656c3b9a32a1ece83d8d07
Marat <kredit73@mail.ru>
2022-12-10 18:03:14 +0300

    Добавлено решение

--- Homework_Seminar_4/Task_25/Program.cs
+++ Homework_Seminar_4/Task_25/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+
+//НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ Math.Pow
+Console.WriteLine("Input A!");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input B!");
+int b = int.Parse(Console.ReadLine());
+
+int result = a;
+
+for(int i =1; i < b; i++)
+{
+    result = result*a;
+    
+} 
+Console.WriteLine($"А в степени B равно =  {result}");
